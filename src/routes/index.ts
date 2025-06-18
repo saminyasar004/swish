ffdf46bf9b94b0576082b4a314c1fb148190da7d
saminyasar004 <yasarsamin57@gmail.com>
2025-06-18 17:06:44 +0600

    dev: landing page route included

---
+++ src/routes/index.ts
@@ -0,0 +1,16 @@
+import Layout from "@/components/common/Layout";
+import Home from "@/pages/Home/Home";
+
+export interface Route {
+	path: string;
+	element: JSX.Element;
+	layout?: JSX.Element;
+}
+
+export const routes = [
+	{
+		path: "/",
+		element: Home,
+		layout: Layout,
+	},
+];
