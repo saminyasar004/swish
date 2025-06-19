14c1a23fc6176db20a139ec31761c064d8f4eaa5
saminyasar004 <yasarsamin57@gmail.com>
2025-06-19 22:31:42 +0600

    dev: categories, post job route added

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,7 @@
 import Layout from "@/components/common/Layout";
+import Categories from "@/pages/Categories/Categories";
 import Home from "@/pages/Home/Home";
+import PostJob from "@/pages/PostJob/PostJob";
 
 export interface Route {
 	path: string;
@@ -11,6 +13,16 @@ export const routes = [
 	{
 		path: "/",
 		element: Home,
+		layout: Layout,
+	},
+	{
+		path: "/categories",
+		element: Categories,
+		layout: Layout,
+	},
+	{
+		path: "/post-job",
+		element: PostJob,
 		layout: Layout,
 	},
 ];
