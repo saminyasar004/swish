dd17dab25f057270f28d0fe69a6dd1f77d0d27da
saminyasar004 <yasarsamin57@gmail.com>
2025-06-19 22:32:36 +0600

    dev: mantine component library added

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,38 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { routes } from "@/routes";
+import { MantineProvider } from "@mantine/core";
+
+import "@mantine/core/styles.css";
 
 const queryClient = new QueryClient();
 
 const App = () => (
-	<QueryClientProvider client={queryClient}>
-		<TooltipProvider>
-			<BrowserRouter>
-				<Routes>
-					{routes.map((route, index) => (
-						<Route
-							key={index}
-							path={route.path}
-							element={
-								route.layout ? (
-									<route.layout>
+	<MantineProvider>
+		<QueryClientProvider client={queryClient}>
+			<TooltipProvider>
+				<BrowserRouter>
+					<Routes>
+						{routes.map((route, index) => (
+							<Route
+								key={index}
+								path={route.path}
+								element={
+									route.layout ? (
+										<route.layout>
+											<route.element />
+										</route.layout>
+									) : (
 										<route.element />
-									</route.layout>
-								) : (
-									<route.element />
-								)
-							}
-						/>
-					))}
-				</Routes>
-			</BrowserRouter>
-		</TooltipProvider>
-	</QueryClientProvider>
+									)
+								}
+							/>
+						))}
+					</Routes>
+				</BrowserRouter>
+			</TooltipProvider>
+		</QueryClientProvider>
+	</MantineProvider>
 );
 
 export default App;
