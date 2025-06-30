30ca72370185a2ebe57d77d94cc081998417cbbc
saminyasar004 <yasarsamin57@gmail.com>
2025-06-30 22:52:37 +0600

    dev: taliwind config changed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,53 +10,73 @@ export default {
 	],
 	prefix: "",
 	theme: {
-		container: {
-			center: true,
-			padding: "2rem",
-			screens: {
-				"2xl": "700px",
-				"3xl": "1000px",
-				"4xl": "1200px",
-				"5xl": "1400px",
-			},
-		},
-		extend: {
-			fontFamily: {
-				poppins: ["Poppins", "sans-serif"],
-			},
-
-			colors: {
-				primary: "#57B576",
-				liquidGreen: "#EEF8F1",
-				secondary: "#1a482a",
-				accent: "#0c381b",
-				accentDark: "#082c14",
-				primaryDark: "#101F41",
-				warning: "#ff523b",
-			},
-			keyframes: {
-				"accordion-down": {
-					from: {
-						height: "0",
-					},
-					to: {
-						height: "var(--radix-accordion-content-height)",
-					},
-				},
-				"accordion-up": {
-					from: {
-						height: "var(--radix-accordion-content-height)",
-					},
-					to: {
-						height: "0",
-					},
-				},
-			},
-			animation: {
-				"accordion-down": "accordion-down 0.2s ease-out",
-				"accordion-up": "accordion-up 0.2s ease-out",
-			},
-		},
-	},
+    	container: {
+    		center: true,
+    		padding: '2rem',
+    		screens: {
+    			'2xl': '700px',
+    			'3xl': '1000px',
+    			'4xl': '1200px',
+    			'5xl': '1400px'
+    		}
+    	},
+    	extend: {
+    		fontFamily: {
+    			poppins: [
+    				'Poppins',
+    				'sans-serif'
+    			]
+    		},
+    		colors: {
+    			primary: '#57B576',
+    			liquidGreen: '#EEF8F1',
+    			secondary: '#1a482a',
+    			accent: '#0c381b',
+    			accentDark: '#082c14',
+    			primaryDark: '#101F41',
+    			warning: '#ff523b'
+    		},
+    		keyframes: {
+    			'accordion-down': {
+    				from: {
+    					height: '0'
+    				},
+    				to: {
+    					height: 'var(--radix-accordion-content-height)'
+    				}
+    			},
+    			'accordion-up': {
+    				from: {
+    					height: 'var(--radix-accordion-content-height)'
+    				},
+    				to: {
+    					height: '0'
+    				}
+    			},
+    			'accordion-down': {
+    				from: {
+    					height: '0'
+    				},
+    				to: {
+    					height: 'var(--radix-accordion-content-height)'
+    				}
+    			},
+    			'accordion-up': {
+    				from: {
+    					height: 'var(--radix-accordion-content-height)'
+    				},
+    				to: {
+    					height: '0'
+    				}
+    			}
+    		},
+    		animation: {
+    			'accordion-down': 'accordion-down 0.2s ease-out',
+    			'accordion-up': 'accordion-up 0.2s ease-out',
+    			'accordion-down': 'accordion-down 0.2s ease-out',
+    			'accordion-up': 'accordion-up 0.2s ease-out'
+    		}
+    	}
+    },
 	plugins: [require("tailwindcss-animate")],
 } satisfies Config;
