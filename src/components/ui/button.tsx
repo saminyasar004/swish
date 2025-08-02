3a3631815ceefb02b540f39c5133bba935113ac4
hProgrammerArif <work.mohammedarif@gmail.com>
2025-08-02 18:10:26 +0600

    dev: add size for md

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -27,6 +27,7 @@ const buttonVariants = cva(
 				default: "h-10 px-5 py-2",
 				xs: "h-6 rounded-md px-3 text-xs",
 				sm: "h-7 rounded-md px-3",
+				md: "h-9 rounded-md px-4",
 				lg: "h-11 rounded-md px-8",
 				icon: "h-10 w-10",
 			},
