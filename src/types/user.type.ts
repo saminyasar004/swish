4d7f9be07aa5564822670830d597daa6414c80cb
hProgrammerArif <work.mohammedarif@gmail.com>
2025-08-13 18:17:57 +0600

    dev: some dummy types

---
+++ src/types/user.type.ts
@@ -0,0 +1,11 @@
+
+export type TUserProfile = {
+    first_name: string;
+    surname: string;
+    email: string;
+    role: string;
+    telephone: string;
+    home_address: string;
+    image: string;
+    city: string;
+};
