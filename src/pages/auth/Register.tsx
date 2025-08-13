09a69584a8e0b0f9f826d7987dbe7ea181f06880
hProgrammerArif <work.mohammedarif@gmail.com>
2025-08-13 18:14:11 +0600

    dev: update register toast

--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -60,12 +60,17 @@ export default function Register() {
 
     try {
       const res = await registerUser(payload).unwrap();
-      console.log({res})
-      toast.success(res.message || "Registered successfully", { id: toastId, duration: 2000 });
+      console.log({ res });
+      toast.success(res.message || "Registered successfully", {
+        id: toastId,
+        duration: 2000,
+      });
       navigate("/login");
     } catch (err: any) {
       console.log({ err });
-      toast.error(err?.data?.message || "Registration failed", { id: toastId });
+      toast.error(err?.data?.email[0] || "Registration failed", {
+        id: toastId,
+      });
     }
   };
 
@@ -87,36 +92,77 @@ export default function Register() {
 
         <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-5">
           <div className="form-group flex flex-col gap-3 lg:px-10">
-            <Label htmlFor="firstName" className="text-secondary">First Name</Label>
-            <Input id="firstName" placeholder="John" {...register("first_name", { required: "First name is required" })} />
-            {errors.first_name && <span className="text-red-500 text-sm">{errors.first_name.message}</span>}
+            <Label htmlFor="firstName" className="text-secondary">
+              First Name
+            </Label>
+            <Input
+              id="firstName"
+              placeholder="John"
+              {...register("first_name", {
+                required: "First name is required",
+              })}
+            />
+            {errors.first_name && (
+              <span className="text-red-500 text-sm">
+                {errors.first_name.message}
+              </span>
+            )}
           </div>
 
           <div className="form-group flex flex-col gap-3 lg:px-10">
-            <Label htmlFor="lastName" className="text-secondary">Last Name</Label>
-            <Input id="lastName" placeholder="Doe" {...register("surname", { required: "Last name is required" })} />
-            {errors.surname && <span className="text-red-500 text-sm">{errors.surname.message}</span>}
+            <Label htmlFor="lastName" className="text-secondary">
+              Last Name
+            </Label>
+            <Input
+              id="lastName"
+              placeholder="Doe"
+              {...register("surname", { required: "Last name is required" })}
+            />
+            {errors.surname && (
+              <span className="text-red-500 text-sm">
+                {errors.surname.message}
+              </span>
+            )}
           </div>
 
           <div className="form-group flex flex-col gap-3 lg:px-10">
-            <Label htmlFor="email" className="text-secondary">Email</Label>
-            <Input id="email" type="email" placeholder="jon.doe@example.com" {...register("email", { required: "Email is required" })} />
-            {errors.email && <span className="text-red-500 text-sm">{errors.email.message}</span>}
+            <Label htmlFor="email" className="text-secondary">
+              Email
+            </Label>
+            <Input
+              id="email"
+              type="email"
+              placeholder="jon.doe@example.com"
+              {...register("email", { required: "Email is required" })}
+            />
+            {errors.email && (
+              <span className="text-red-500 text-sm">
+                {errors.email.message}
+              </span>
+            )}
           </div>
 
           <div className="form-group flex flex-col gap-3 lg:px-10">
-            <Label htmlFor="telephone" className="text-secondary">Telephone</Label>
+            <Label htmlFor="telephone" className="text-secondary">
+              Telephone
+            </Label>
             <PhoneInput
               country={"ma"}
               value={phone}
               onChange={setPhone}
               placeholder="+ 123 456 7890"
-              inputStyle={{ height: "48px", width: "100%", borderRadius: "8px" }}
+              inputStyle={{
+                height: "48px",
+                width: "100%",
+                borderRadius: "8px",
+              }}
             />
           </div>
 
           <div className="form-group flex flex-col gap-3 lg:px-10">
-            <Label htmlFor="password" className="text-secondary">Password</Label>
+            <Label htmlFor="password" className="text-secondary">
+              Password
+            </Label>
             <div className="relative w-full">
               <Input
                 id="password"
@@ -125,18 +171,33 @@ export default function Register() {
                 className="pr-16"
                 {...register("password", { required: "Password is required" })}
               />
-              <Separator className="bg-primary/50 w-0.5 h-[70%] absolute top-1/2 right-12 -translate-y-1/2" orientation="vertical" />
+              <Separator
+                className="bg-primary/50 w-0.5 h-[70%] absolute top-1/2 right-12 -translate-y-1/2"
+                orientation="vertical"
+              />
               {showPassword ? (
-                <EyeOff onClick={() => setShowPassword(false)} className="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer text-primary/50" />
+                <EyeOff
+                  onClick={() => setShowPassword(false)}
+                  className="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer text-primary/50"
+                />
               ) : (
-                <Eye onClick={() => setShowPassword(true)} className="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer text-primary/50" />
+                <Eye
+                  onClick={() => setShowPassword(true)}
+                  className="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer text-primary/50"
+                />
               )}
             </div>
-            {errors.password && <span className="text-red-500 text-sm">{errors.password.message}</span>}
+            {errors.password && (
+              <span className="text-red-500 text-sm">
+                {errors.password.message}
+              </span>
+            )}
           </div>
 
           <div className="form-group flex flex-col gap-3 lg:px-10">
-            <Label htmlFor="confirmPassword" className="text-secondary">Confirm Password</Label>
+            <Label htmlFor="confirmPassword" className="text-secondary">
+              Confirm Password
+            </Label>
             <div className="relative w-full">
               <Input
                 id="confirmPassword"
@@ -145,17 +206,31 @@ export default function Register() {
                 className="pr-16"
                 {...register("confirmPassword", {
                   required: "Confirm your password",
-                  validate: value => value === watch("password") || "Passwords do not match"
+                  validate: (value) =>
+                    value === watch("password") || "Passwords do not match",
                 })}
               />
-              <Separator className="bg-primary/50 w-0.5 h-[70%] absolute top-1/2 right-12 -translate-y-1/2" orientation="vertical" />
+              <Separator
+                className="bg-primary/50 w-0.5 h-[70%] absolute top-1/2 right-12 -translate-y-1/2"
+                orientation="vertical"
+              />
               {showConfirmPassword ? (
-                <EyeOff onClick={() => setShowConfirmPassword(false)} className="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer text-primary/50" />
+                <EyeOff
+                  onClick={() => setShowConfirmPassword(false)}
+                  className="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer text-primary/50"
+                />
               ) : (
-                <Eye onClick={() => setShowConfirmPassword(true)} className="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer text-primary/50" />
+                <Eye
+                  onClick={() => setShowConfirmPassword(true)}
+                  className="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer text-primary/50"
+                />
               )}
             </div>
-            {errors.confirmPassword && <span className="text-red-500 text-sm">{errors.confirmPassword.message}</span>}
+            {errors.confirmPassword && (
+              <span className="text-red-500 text-sm">
+                {errors.confirmPassword.message}
+              </span>
+            )}
           </div>
 
           <div className="form-group w-full flex items-center justify-center lg:px-10">
@@ -172,7 +247,11 @@ export default function Register() {
         </div>
 
         <div className="form-group w-full flex items-center justify-center lg:px-10">
-          <Button className="w-full flex gap-3 items-center rounded-full" size="lg" variant="transparent">
+          <Button
+            className="w-full flex gap-3 items-center rounded-full"
+            size="lg"
+            variant="transparent"
+          >
             <img src={GoogleLogo} alt="Google" className="h-5" />
             Continue with Google
           </Button>
@@ -190,7 +269,6 @@ export default function Register() {
     </section>
   );
 }
-
 
 // import LogoWhite from "@/assets/images/logo-white.svg";
 // import LogoBlack from "@/assets/images/logo-black.svg";
