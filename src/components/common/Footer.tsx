373ee2b0425da48aedb7e7067409bf1735219a96
hProgrammerArif <work.mohammedarif@gmail.com>
2025-09-08 13:11:04 +0600

    dev: fix footer

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -2,66 +2,101 @@ import Logo from "@/assets/images/logo-white.svg";
 import { Facebook, Instagram, Linkedin } from "lucide-react";
 import { FaTiktok } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
 
 export default function Footer() {
   const footerRoutes = [
+    { name: "Help Guide", url: "/#" },
     { name: "Contact", url: "/contact" },
-    // { name: "Post a job", url: "/post-job" },
-    // { name: "Register Company", url: "/register-company" },
-    { name: "Who is swish?", url: "/#" },
+    { name: "Post a job", url: "/post-job" },
+    { name: "Register Company", url: "/register-company" },
   ];
 
-  const footerLinks = [
-    { name: "Terms of us", url: "/term" },
-    { name: "Terms of service", url: "/service" },
-    // { name: "Term", url: "/term" },
-    // { name: "Cookie Settings", url: "/cookie-settings" },
+  const termCondition = [
+    { name: "Privacy Statement", url: "#" },
+    { name: "• Sitemap", url: "#" },
+    { name: "• Term", url: "#" },
+    { name: "• Cookie Settings", url: "#" },
   ];
 
   const quickLinks = [
     {
-      name: "-How does swish work?",
-      url: "https://mittanbud.no/privatperson/startguide/slik-fungerer-mittanbud",
+      name: "How does swish work?",
+      url: "/",
     },
     {
-      name: "-Safety & quality assurance",
-      url: "https://mittanbud.no/privatperson/startguide/slik-velger-du-riktig-haandverker",
+      name: "Safety & quality assurance",
+      url: "/",
     },
     {
-      name: "-Reviews on Swish",
-      url: "https://mittanbud.no/proff/startguide/evalueringer",
+      name: "Reviews on Swish",
+      url: "/",
     },
     {
-      name: "-How to write a good job description",
-      url: "https://mittanbud.no/privatperson/startguide/skriv-god-oppdragsbeskrivelse",
+      name: "How to write a good job description",
+      url: "/",
     },
     {
-      name: "-Frequently asked questions",
-      url: "https://mittanbud.no/privatperson/startguide/faq",
+      name: "Frequently asked questions",
+      url: "/",
     },
     {
       name: "Startguide",
-      url: "https://mittanbud.no/privatperson/startguide",
-    },
-  ];
-
-  const forBusinessLinks = [
-    {
-      name: "Register your company",
-      url: "https://mittanbud.no/for-bedrifter?slug=registrer-bedrift",
-    },
-
-    {
-      name: "Startguide",
-      url: "https://mittanbud.no/proff/startguide",
+      url: "/",
     },
   ];
   // const quickLinks = [
-  //   { name: "- New Construction & Permits", url: "" },
-  //   { name: "Exterior & Garden", url: "" },
-  //   { name: "Trades & Specialized Crafts", url: "" },
-  //   { name: "Home Services & Installations", url: "" },
-  //   { name: "Indoor Renovation", url: "" },
+  //   {
+  //     name: "How does swish work?",
+  //     url: "/",
+  //   },
+  //   {
+  //     name: "Safety & quality assurance",
+  //     url: "/",
+  //   },
+  //   {
+  //     name: "Reviews on Swish",
+  //     url: "/",
+  //   },
+  //   {
+  //     name: "How to write a good job description",
+  //     url: "/",
+  //   },
+  //   {
+  //     name: "Frequently asked questions",
+  //     url: "/",
+  //   },
+  //   {
+  //     name: "Startguide",
+  //     url: "/",
+  //   },
+  // ];
+
+  const forBusinessLinks = [
+    {
+      name: "09:00 - 16:00 on weekdays.\nClosed on weekends",
+      url: "#",
+    },
+    {
+      name: "Send e-post: info@swish.ma.no",
+      url: "#",
+    },
+    {
+      name: "Ring oss: 22 00 09 30",
+      url: "#",
+    },
+  ];
+  // const forBusinessLinks = [
+  //   {
+  //     name: "Register your company",
+  //     url: "register-company",
+  //   },
+
+  //   {
+  //     name: "Startguide",
+  //     url: "/",
+  //   },
   // ];
 
   const socialLinks = [
@@ -86,104 +121,124 @@ export default function Footer() {
   return (
     <footer className="text-white">
       <div className="bg-accent text-white">
-        <div className="py-16 px-2 w-full max-w-[1800px] mx-auto grid md:grid-cols-3 lg:grid-cols-6 lg:gap-4 gap-12 justify-center items-center text-center md:justify-start md:items-start md:text-left">
-          {/* <div className="flex items-center justify-center md:justify-start lg:col-span-1 ">
+        <div className="py-16 p-2 md:px-6 xl:px-2 w-full max-w-[1800px] mx-auto grid grid-cols-1 md:grid-cols-3  xl:grid-cols-5 lg:gap-4 gap-12">
+          <div className="flex items-center justify-center md:justify-start ">
             <img src={Logo} alt="swish.ma" className="max-w-full" />
-          </div> */}
-
-          <div className="lg:col-span-2 flex flex-col gap-2  ">
-            <h6 className="font-semibold">About Swish</h6>
-
-            {footerRoutes.map((route, index) => (
-              <Link
-                key={index}
-                to={route.url}
-                className="transition-all duration-300 hover:underline text-sm"
-              >
-                {route.name}
-              </Link>
-            ))}
           </div>
 
-          <div className="lg:col-span-2 flex flex-col gap-2 ">
-            <h6 className="font-semibold">For individuals</h6>
-            <Link
-              target="_blank"
-              to={`https://mittanbud.no/privatperson/startguide/hjelpesenter`}
-              className="font-semibold"
-            >
-              Help center
-            </Link>
+          <div className="flex flex-col  justify-center items-center text-center gap-2  md:justify-start md:items-start md:text-left ">
+            <h5 className="font-semibold text-primary text-lg ">swish.ma</h5>
 
-            {quickLinks.map((quickLink, index) => (
-              <Link
-                target="_blank"
-                key={index}
-                to={quickLink.url}
-                className="transition-all duration-300 hover:underline text-sm"
-              >
-                {quickLink.name}
-              </Link>
-            ))}
-          </div>
-
-          <div className="lg:col-span-2 flex flex-col gap-2">
-            <h6 className="font-semibold">For businesses</h6>
-
-            {forBusinessLinks.map((quickLink, index) => (
-              <Link
-                target="_blank"
-                key={index}
-                to={quickLink.url}
-                className="transition-all duration-300 hover:underline text-sm"
-              >
-                {quickLink.name}
-              </Link>
-            ))}
-
-            {/* <p className="text-sm lg:w-[50%]">
-              09:00 - 16:00 on weekdays. closed on weekends.
-            </p>
-
-            <p className="text-sm lg:w-[50%]">Send e-post: info@swish.ma.no</p>
-
-            <p className="text-sm lg:w-[50%]">Ring oss: 22 00 09 30</p> */}
-          </div>
-        </div>
-      </div>
-
-      <div className="py-10 bg-accentDark text-white justify-center items-center text-center md:justify-start md:items-start md:text-left">
-        <div className="container grid md:grid-cols-2 gap-12">
-          <div className="flex flex-col gap-3">
-            <div className="flex">
-              <img src={Logo} alt="swish.ma" className="max-w-full" />
-            </div>
-
-            <p className="text-sm font-normal">Swish by Zoopy SARL © 2025</p>
-
-            <div className="w-full flex items-center justify-center md:justify-start gap-3">
-              {footerLinks.map((link, index) => (
+            <div className="flex flex-col">
+              {footerRoutes.map((route, index) => (
                 <Link
-                  to={link.url}
-                  target="_blank"
-                  className="transition-all duration-300 hover:underline text-sm font-normal"
                   key={index}
+                  to={route.url}
+                  className="transition-all duration-300 hover:underline text-sm mt-2 font-light"
                 >
-                  {link.name}
+                  {route.name}
                 </Link>
               ))}
             </div>
           </div>
 
-          <div className="w-full flex items-start justify-center gap-3">
-            {socialLinks.map((socialLink, index) => (
+          <div className="flex flex-col gap-2 justify-center items-center text-center md:justify-start md:items-start md:text-left">
+            <h5 className="font-semibold text-primary text-lg ">Quick Links</h5>
+
+            <div className="flex flex-col">
+              {quickLinks.map((quickLink, index) => (
+                <Link
+                  // target="_blank"
+                  key={index}
+                  to={quickLink.url}
+                  className="transition-all duration-300 hover:underline text-sm mt-2 font-light"
+                >
+                  &nbsp;{quickLink.name}
+                </Link>
+              ))}
+            </div>
+          </div>
+
+          <div className="flex flex-col col-span-1 gap-2 justify-center items-center text-center md:justify-start md:items-start md:text-left">
+            <h6 className="font-semibold text-primary text-lg">
+              Customer Service
+            </h6>
+
+            <div className="flex flex-col">
+              {forBusinessLinks.map((quickLink, index) => (
+                <Link
+                  // target="_blank"
+                  key={index}
+                  to={quickLink.url}
+                  className="transition-all duration-300 hover:underline text-sm whitespace-pre-line mt-2 font-light"
+                >
+                  {quickLink.name}
+                </Link>
+              ))}
+            </div>
+          </div>
+
+          <div className="gap-4 w-full md:col-span-2 xl:col-span-1">
+            {/* Newsletter Section */}
+            <div className="flex flex-col gap-2 text-center items-center md:items-start md:text-left">
+              <h6 className="font-semibold text-primary text-lg">Newsletter</h6>
+
+              <p className="text-sm font-light">
+                Stay close to nature! Subscribe now for fresh harvest updates
+                and special farm deals.
+              </p>
+
+              {/* Input + Button inline */}
+              <div className="flex w-full max-w-sm rounded-md overflow-hidden border border-primary mt-2">
+                <Input
+                  type="email"
+                  placeholder="Enter your email"
+                  className="flex-1 border-none focus:ring-0 px-3 text-sm bg-transparent lg:h-12"
+                />
+                <Button className="bg-primary text-white  px-4 rounded-none md:h-12 ">
+                  Subscribe
+                </Button>
+              </div>
+            </div>
+
+            {/* Social Links */}
+            <div className="w-full flex flex-col text-center md:items-start md:justify-center gap-3 mt-4">
+              <h6 className="font-semibold text-primary text-lg">Follow Us</h6>
+              <div className="flex flex-wrap justify-center items-center gap-3">
+                {socialLinks.map((socialLink, index) => (
+                  <Link
+                    to={socialLink.url}
+                    target="_blank"
+                    key={index}
+                    className="transition-all duration-300 hover:bg-primary hover:border-primary border border-primary rounded-full p-2"
+                  >
+                    <socialLink.icon size={16} />
+                  </Link>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="py-6 bg-accentDark text-white">
+        <div className="grid md:grid-cols-2 gap-6 justify-between items-center px-4 w-full max-w-[1800px] mx-auto">
+          {/* Left Section */}
+          <div className="flex flex-col gap-2 text-center md:text-left">
+            <p className="text-sm font-normal">
+              Swish by Zoopy SARL © {new Date().getFullYear()}
+            </p>
+          </div>
+
+          {/* Right Section */}
+          <div className="flex flex-wrap items-center justify-center md:justify-end gap-4">
+            {termCondition.map((route, index) => (
               <Link
-                to={socialLink.url}
-                target="_blank"
-                className="transition-all duration-300 hover:bg-primary hover:border-primary text-sm border border-white rounded-full p-2"
                 key={index}
+                to={route.url}
+                className="transition-all duration-300 hover:underline text-sm font-light"
               >
-                <socialLink.icon size={16} />
+                {route.name}
               </Link>
             ))}
           </div>
