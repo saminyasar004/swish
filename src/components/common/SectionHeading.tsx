044c40ad9949a46da1d3823f4616775bdb83b8d9
hProgrammerArif <work.mohammedarif@gmail.com>
2025-09-08 13:12:00 +0600

    dev: improve resuable section heading

--- src/components/common/SectionHeading.tsx
+++ src/components/common/SectionHeading.tsx
@@ -6,11 +6,11 @@ export default function SectionHeading({
 	description?: string;
 }) {
 	return (
-		<div className="w-full flex items-center justify-center text-center flex-col gap-3 ">
-			<h2 className="text-primaryDark font-semibold text-2xl md:text-3xl mb-2">{title}</h2>
+		<div className="w-full flex items-center justify-center text-center flex-col gap-4 ">
+			<h2 className="text-primaryDark font-bold text-2xl md:text-3xl lg:text-4xl ">{title}</h2>
 
 			{description && (
-				<p className="w-[80%]  md:w-[50%] text-base text-[#404C67] text-center">
+				<p className="w-[90%]  lg:w-[60%] text-base text-[#404C67] text-center">
 					{description}
 				</p>
 			)}
